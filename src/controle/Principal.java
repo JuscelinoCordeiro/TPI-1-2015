907dac345846a5854ae4ec761d44ecbb2e3618ff
JuscelinoCordeiro <jfilhodev@gmail.com>
2015-04-11 21:10:52 -0300

    commit 2 - lendo dados do txt - 11-04-15 21:13

--- src/controle/Principal.java
+++ src/controle/Principal.java
@@ -5,16 +5,47 @@
  */
 package controle;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author apolo
  */
 public class Principal {
+
     public static void main(String[] args) {
-        System.out.println("Teste java");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Execução " + i);
-            System.out.println("\ngit config --global --list");
+        System.out.println("TESTE DE EXECUÇÃO");
+//        Scanner ler = new Scanner(System.in);
+        
+//        System.out.println("Informe o nome do arquivo: \n");
+//        String nome = ler.nextLine();
+        
+        String arquivoEntrada = "/home/apolo/Dropbox/UFF-SI/TPI-1/Escalonador de Tarefas/src/entrada.txt";
+        String arquivoSaida = "/home/apolo/Dropbox/UFF-SI/TPI-1/Escalonador de Tarefas/src/entrada.txt";
+        
+        System.out.println("\nConteudo do arquivo: \n");
+        //LENDO DO ARQUIVO DE ENTRADA
+        try {
+            FileReader arq1 = new FileReader(arquivoEntrada);
+            BufferedReader lerArq = new BufferedReader(arq1);
+
+            String linha = lerArq.readLine(); // lê a primeira linha 
+            // a variável "linha" recebe o valor "null" quando o processo 
+            // de repetição atingir o final do arquivo texto 
+            while (linha != null) {
+                System.out.printf("%s\n", linha);
+                linha = lerArq.readLine(); // lê da segunda até a última linha
+            }
+
+            arq1.close();
+
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n",
+                    e.getMessage());
         }
+        System.out.println("");
     }
 }
