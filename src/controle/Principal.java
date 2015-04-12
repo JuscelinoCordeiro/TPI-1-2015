f82c2f32c1a75b55b9598271e58cb5501db23e7e
JuscelinoCordeiro <jfilhodev@gmail.com>
2015-04-12 20:03:00 -0300

    colocando todos os processos na fila

--- src/controle/Principal.java
+++ src/controle/Principal.java
@@ -24,12 +24,12 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println("TESTE DE EXECUÇÃO");
- 
-        List <Processo> filaDeProcessos = new LinkedList<Processo>();
+
+        List<Processo> filaDeProcessos = new LinkedList<Processo>();
         Processo p1 = new Processo();
 
         String arquivoEntrada = "/home/apolo/Dropbox/UFF-SI/TPI-1/Escalonador de Tarefas/src/arquivos/entrada.txt";
-        String arquivoSaida = "/home/apolo/Dropbox/UFF-SI/TPI-1/Escalonador de Tarefas/src/arquivos/saida.txt";
+        String arquivoSaida   = "/home/apolo/Dropbox/UFF-SI/TPI-1/Escalonador de Tarefas/src/arquivos/saida.txt";
 
         System.out.println("\nConteudo do arquivo: \n");
 
@@ -48,71 +48,122 @@ public class Principal {
 
 //            gravarArq.printf("ID TC TP P\n");
             String informacao = ""; //string para armazenar a informação lida da linha
-            char c = ' ';
-            int i = 0; //indice na linha
-            int j = 0; //indice de atributo do processo para uso no switch
-
-            while (i <= linhaLida.length()) {
-                if (i == 0) { //primeiro cacacter da linha
-                    c = linhaLida.charAt(i);
-                    informacao = Character.toString(c);
-                } else {
-                    if (i < linhaLida.length()) {
-                        c = linhaLida.charAt(i);
-                    }
-
-                    if (c == ' ' || i == linhaLida.length()) {
-                        System.out.println("\ni =" + i);
-                        ++j;
-                        System.out.println("j = " + j);
-                        System.out.println("informação " + informacao);
-                        switch (j) {
-                            case 1:
-                                p1.setId(Integer.parseInt(informacao));
-                                break;
-                            case 2:
-                                p1.setTmpChegada(Integer.parseInt(informacao));
-                                break;
-                            case 3:
-                                p1.setDuracao(Integer.parseInt(informacao));
-                                break;
-                            case 4:
-                                p1.setPrioridade(Integer.parseInt(informacao));
-                                break;
-                            default:
-                                throw new AssertionError();
-                        }
-                        informacao = "";
-                        
-                    } else {
-                        informacao += Character.toString(c);                   
-                    }
-                }
-                i++;
-            }
-            filaDeProcessos.add(p1);
-            System.out.println("tamanho da fila: " + filaDeProcessos.size());
-            DetalharProcesso.detalhes(filaDeProcessos.get(0));
-            /*
-             for (int i = 0; i < linhaLida.length(); i++) {
-             c = linhaLida.charAt(i);
-
-             if (c == ' ') {
-             System.out.println("É um espaço");
-             } else {
-             informacao += Character.toString(c);
-             System.out.println(c);
-             }
-             }
-             */
-//            while (linha != null) {                                
-//                gravarArq.println(linha);
-//                
-//                System.out.printf("%s\n", linha);
-//                linha = lerArq.readLine(); // lê da segunda até a última linha
+//            char c = ' ';
+//            int i = 0; //indice na linha
+//            int j = 0; //indice de atributo do processo para uso no switch
+//
+//            while (i <= linhaLida.length()) {
+//                if (i == 0) { //primeiro cacacter da linha
+//                    c = linhaLida.charAt(i);
+//                    informacao = Character.toString(c);
+//                } else {
+//                    if (i < linhaLida.length()) {
+//                        c = linhaLida.charAt(i);
+//                    }
+//
+//                    if (c == ' ' || i == linhaLida.length()) {
+//                        System.out.println("\ni =" + i);
+//                        ++j;
+//                        System.out.println("j = " + j);
+//                        System.out.println("informação " + informacao);
+//                        switch (j) {
+//                            case 1:
+//                                p1.setId(Integer.parseInt(informacao));
+//                                break;
+//                            case 2:
+//                                p1.setTmpChegada(Integer.parseInt(informacao));
+//                                break;
+//                            case 3:
+//                                p1.setDuracao(Integer.parseInt(informacao));
+//                                break;
+//                            case 4:
+//                                p1.setPrioridade(Integer.parseInt(informacao));
+//                                break;
+//                            default:
+//                                throw new AssertionError();
+//                        }
+//                        informacao = "";
+//
+//                    } else {
+//                        informacao += Character.toString(c);
+//                    }
+//                }
+//                i++;
 //            }
+//            filaDeProcessos.add(p1);
+////            System.out.println("tamanho da fila: " + filaDeProcessos.size());
+//            DetalharProcesso.detalhes(filaDeProcessos.get(0));
+
+//Processo p2 = new Processo();
+//            linhaLida = lerArq.readLine();
+            while (linhaLida != null) {
+                Processo p2 = new Processo();
+                char z = ' ';
+                int k = 0; //indice na linha
+                int l = 0; //indice de atributo do processo para uso no switch
+//                linhaLida = lerArq.readLine(); // lê da segunda até a última linha
+//                System.out.println(linhaLida+"\n");
+                while (k <= linhaLida.length()) {
+                    if (k == 0) { //primeiro cacacter da linha
+                        z = linhaLida.charAt(k);
+                        informacao = Character.toString(z);
+                    } else {
+                        if (k < linhaLida.length()) {
+                            z = linhaLida.charAt(k);
+                        }
+
+                        if (z == ' ' || k == linhaLida.length()) {
+//                            System.out.println("\nk =" + k);
+                            ++l;
+//                            System.out.println("l = " + l);
+//                            System.out.println("informação " + informacao);
+                            switch (l) {
+                                case 1:
+                                    p2.setId(Integer.parseInt(informacao));
+                                    break;
+                                case 2:
+                                    p2.setTmpChegada(Integer.parseInt(informacao));
+                                    break;
+                                case 3:
+                                    p2.setDuracao(Integer.parseInt(informacao));
+                                    break;
+                                case 4:
+                                    p2.setPrioridade(Integer.parseInt(informacao));
+                                    break;
+                                default:
+//                                    throw new AssertionError();
+                                    System.out.println("k = " + k);
+                                    System.out.println("l = " + l);
+
+                            }
+                            informacao = "";
+
+                        } else {
+                            informacao += Character.toString(z);
+                        }
+                    }
+                    k++;
+                }
+                filaDeProcessos.add(p2);
+                linhaLida = lerArq.readLine();
+            }
             arq2.close();
             arq1.close();
+
+            System.out.println("tamanho da fila = " + filaDeProcessos.size());
+
+            for (int i = 0; i < filaDeProcessos.size(); i++) {
+                System.out.println("i = " + i);
+                DetalharProcesso.detalhes(filaDeProcessos.get(i));
+
+                gravarArq.println("Dados do processo: " + i);
+                gravarArq.printf("ID TC TP P\n");
+                gravarArq.print(filaDeProcessos.get(i).getId());
+                gravarArq.print(" " + filaDeProcessos.get(i).getTmpChegada());
+                gravarArq.print(" " + filaDeProcessos.get(i).getDuracao());
+                gravarArq.print(" " + filaDeProcessos.get(i).getPrioridade());
+                gravarArq.println("\n ================================\n");
+            }
 
         } catch (IOException e) {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
