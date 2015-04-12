39edbcd004e65add3de0c8d910f37ce2a1471862
JuscelinoCordeiro <jfilhodev@gmail.com>
2015-04-12 10:03:22 -0300

    percorrendo a linha e concatenado os chars 2

---
+++ src/model/processo.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author apolo
+ */
+public class processo {
+    private int id;
+    private int tmpChegada;
+    private int duracao;
+    private int prioridade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTmpChegada() {
+        return tmpChegada;
+    }
+
+    public void setTmpChegada(int tmpChegada) {
+        this.tmpChegada = tmpChegada;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+    
+    
+}
